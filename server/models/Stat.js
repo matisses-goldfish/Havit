bca0c224ef95564b693278cb9e1957fe07f5fa87
boocipher <boocipher@gmail.com>
2022-03-12 21:48:18 -0600

    Adds models

---
+++ server/models/Stat.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const statSchema = new Schema({
+  enteredDate: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+  },
+  habit: {
+    type: Schema.Types.ObjectId,
+    ref: 'Habit'
+  },
+  value: {
+      type: Number,
+      required:true
+  }
+});
+
+const Stat = mongoose.model('Stat', statSchema);
+
+module.exports = Stat;
