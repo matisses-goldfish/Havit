862df8d0ef8cb07b241b7f6a6c7d06dd136a01fd
sahana <sahanamai@gmail.com>
2022-03-14 12:12:07 -0500

    signup page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_PROFILE } from '../utils/mutations';
+const Signup = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+    const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addProfile({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.addProfile.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    return (
+        <main>
+            <div>
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </main>
+    );
+};
+
+export default Signup;
