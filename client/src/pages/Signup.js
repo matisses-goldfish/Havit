2948787959919220f5c2432dbd6bd8bbd2c4b1b3
sahana <sahanamai@gmail.com>
2022-03-14 13:29:18 -0500

    mui signup form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_PROFILE } from '../utils/mutations';
+import { Grid, Paper } from '@material-ui/core';
+import AddCircleOutlinedPutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 const Signup = () => {
+    const paperStyle = { padding: '30px 20px', width: 300, margin: '20px auto' }
+    const headerStyle = { margin: 0 }
     const [formState, setFormState] = useState({
         name: '',
         email: '',
@@ -34,20 +38,37 @@ const Signup = () => {
         }
     };
     return (
-        <main>
-            <div>
+        <Grid>
+            <Paper elevation={20} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar>
+                        <AddCircleOutlinedPutlinedIcon />
+                    </Avatar>
 
+                    <h2 style={headerStyle}>Sign Up</h2>
+                    <Typography variant='caption' gutterBottom>Please fill this account to create an account!</Typography>
+                </Grid>
+                <form onSubmit={handleFormSubmit}>
+                    <TextField fullWidth label='Username' placeholder="Enter your name"
+                        type='text'
+                        value={formState.name}
+                        onChange={handleChange} />
+                    <TextField fullWidth label='Email'
+                        value={formState.email}
+                        onChange={handleChange} />
+                    <TextField fullWidth label='Password'
+                        placeholder="******"
+                        type="password"
+                        value={formState.password}
+                        onChange={handleChange} />
+                    <TextField fullWidth label='Confirm Password' placeholder="******"
+                        value={formState.password}
+                        onChange={handleChange} />
+                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
 
-
-
-
-
-
-
-
-
-            </div>
-        </main>
+                </form>
+            </Paper>
+        </Grid>
     );
 };
 
