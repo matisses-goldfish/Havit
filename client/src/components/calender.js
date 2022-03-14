cf5e3967b8a269030333c34285a37bc5d923f6dd
sahana <sahanamai@gmail.com>
2022-03-14 11:19:11 -0500

    commented few code to change

--- client/src/components/calender.js
+++ client/src/components/calender.js
@@ -9,7 +9,7 @@ import search from '../utils/Api';
 
 const SearchResultContainer = () => {
   // Declare a new state variable, "results"
-  const [results, setResults] = useState([]);
+  // const [results, setResults] = useState([]);
 
   // Method to get search results and set state
   const getCalender = async () => {
@@ -18,8 +18,7 @@ const SearchResultContainer = () => {
     setResults(response.data.data);
   };
 
-  // We want to run this method when the component first loads so that we have images of kittens to display
-  // The second argument is the dependency array. This means that this method will only run when the component first loads
+  
   useEffect(() => {
     getCalender();
   }, []);
