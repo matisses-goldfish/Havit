6197c8b9d9fc277f452f08df6baef6c81899f12b
sahana <sahanamai@gmail.com>
2022-03-15 12:46:21 -0500

    refer code from mui

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,25 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Link from '@material-ui/core/Link';
 import Auth from '../../utils/auth';
 
-function NavTabs() {
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
-  const [pageTab, setPageTab] = useState('habit');
+export default function CenteredTabs() {
+  const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
+  const [value, setValue] = useState(0);
 
-  const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
-  const { data } = useQuery(QUERY_ME);
-
   return (
-    <div className="container-fluid">
-      {Auth.loggedIn() ? (
-
-    </div>
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Home" />
+       
+        <Link href="#" onClick={preventDefault}>
+          Home
+        </Link>
+       
+        
+       
+        <Tab label="Login" />
+        <Link href="#" onClick={preventDefault}>
+          Login
+        </Link>
+        <Tab label="Habit" />
+        <Link href="#" onClick={preventDefault}>
+          Habit
+        </Link>
+      </Tabs>
+    </Paper>
   );
 }
+  
 
-export default NavTabs;
+  
