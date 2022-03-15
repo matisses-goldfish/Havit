99e2afd1c92a30285773b17f2fcdfa0484a60d13
sahana <sahanamai@gmail.com>
2022-03-15 13:41:36 -0500

    added container,grid ,typography

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 // import { useLocation, useNavigate } from 'react-router-dom';
-
+import { Container, Grid, Typography } from "@material-ui/core";
 const Footer = () => {
   // const location = useLocation();
   // const navigate = useNavigate();
+  //To do need to check on the page and need to add style on container, grid or typography
   return (
     <footer className="footer">
-    <h4>&copy; {new Date().getFullYear()} - Habit-Tracker</h4>
+      <Container maxWidth="lg">
+      <Grid container spacing={3} justify="center">
+      <Typography>
+      <h4>&copy; {new Date().getFullYear()} - Habit-Tracker</h4>
+      </Typography>
+     
+        </Grid>
+      </Container>
+   
     </footer>
   );
 };
