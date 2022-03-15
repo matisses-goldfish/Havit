ac40953b92de464d284ed8345b476c5867fba2d2
matisses goldfish <honoregn@gmail.com>
2022-03-15 11:30:44 -0500

    created draft of habit list for dashboard

--- client/src/components/HabitList/index.js
+++ client/src/components/HabitList/index.js
@@ -1,21 +1,52 @@
-// TODO: change habitAuthor
+// TODO: change code based on model and mutation input
 import React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Checkbox from '@mui/material/Checkbox';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 
 const HabitList = ({
+
   habits,
-  title,
-  showTitle = true,
-  showUsername = true,
+  // name,
+  // showName = true,
+  // showUsername = true,
 }) => {
   if (!habits.length) {
     return <h3>No habits have been created</h3>;
   }
 
-  // TODO: Create Habit list styling and content
   return (
     <div>
-      
+      {habits &&
+        habits.map((habit) => (
+          // TODO: would we want the type to be correlated with an icon?
+          // TODO: adjust maxWidth
+      <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          {habit.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {habit.createdDate}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {/* TODO: Change based on models */}
+          {habit.goalValue}
+            {/* TODO: possible have it effect color??? */}
+          </Typography>
+          <FormControlLabel
+          value="end"
+          control={<Checkbox />}
+        />
+        </CardContent>
+      </CardActionArea>
+    </Card>
+        ))}
     </div>
   );
 };
