bef3b22d43199d320121de93c3f72d7d0a7f377f
lizbett <rlizbett21@gmail.com>
2022-03-15 01:32:38 -0500

    updated newhabits.js

--- client/src/pages/Newhabit.js
+++ client/src/pages/Newhabit.js
@@ -2,31 +2,27 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-import Slider from '@mui/material/Slider';
 
 function Newhabit() {
     return (
         <Box
             sx={{
                 display: 'flex',
+                flexWrap: 'wrap',
                 '& > :not(style)': {
                     m: 1,
-                    width: 128,
-                    height: 128,
+                    width: 350,
+                    height: 300,
                 },
             }}
         >
-            <Paper elevation={0}
-            />
-            <h1>Welcome back...</h1>
-            <h3>Question 1?</h3>
-            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
-            <h3>Question 2?</h3>
-            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
-            <h3>Question 3?</h3>
-            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
-            <h3>Text Iput Question</h3>
-            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <h3>Creat a new goal</h3>
+            <TextField id="outlined-basic" label="Question" variant="outlined" />
+            <TextField id="outlined-basic" label="Type" variant="outlined" />
+            <TextField id="outlined-basic" label="Date" variant="outlined" />
+            <Paper />
+            {/*Import Calendar*/}
+            <Paper elevation={3} />
         </Box>
     );
 }
