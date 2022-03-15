71e23326ae6a212eb23b3b979be324e66ea253b1
lizbett <rlizbett21@gmail.com>
2022-03-15 01:25:49 -0500

    updated progress

--- client/src/pages/Progress.js
+++ client/src/pages/Progress.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+import Slider from '@mui/material/Slider';
 
 function Progress() {
     return (
@@ -20,32 +16,17 @@ function Progress() {
                 },
             }}
         >
-            <Paper variant="outlined">
-                <h3>Welcome back</h3>
-                <h4>Daily Habits:</h4>
-                <FormControlLabel
-                    value="start"
-                    control={<Checkbox />}
-                    label="Begginer"
-                    labelPlacement="start"
-                />
-                <FormControlLabel
-                    value="start"
-                    control={<Checkbox />}
-                    label="Medium"
-                    labelPlacement="start"
-                />
-                <FormControlLabel
-                    value="start"
-                    control={<Checkbox />}
-                    label="Mater"
-                    labelPlacement="start"
-                />
-            </Paper>
-            <Paper variant="outlined" square>
-                <h3>Stats</h3>
-                {/*Import Stats Chart*/}
-            </Paper>
+            <Paper elevation={0}
+            />
+            <h1>Welcome back...</h1>
+            <h3>Question 1?</h3>
+            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+            <h3>Question 2?</h3>
+            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+            <h3>Question 3?</h3>
+            <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+            <h3>Text Iput Question</h3>
+            <TextField id="filled-basic" label="Filled" variant="filled" />
         </Box>
     );
 }
