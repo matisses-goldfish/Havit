c15ba76c7b21c3dfacfe89d06f37888d5bbebf2c
sahana <sahanamai@gmail.com>
2022-03-16 19:34:56 -0500

    deleted link from mui

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Link from '@material-ui/core/Link';
+
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 // import Login from '../../pages/Login';
@@ -54,11 +54,11 @@ const navTab = () => {
             </Link>
             
             <Link className="btn btn-lg btn-light m-2" to="/login">
-            <Tab label="Login" />
+            <Tab  />
             </Link>
           
             <Link className="btn btn-lg btn-light m-2" to="/signup">
-            <Tab label="Signup" />
+            <Tab  />
             </Link>
 
 
