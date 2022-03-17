ffe6a439e5e966ec9f886e2741903dfa2c4c94c0
sahana <sahanamai@gmail.com>
2022-03-16 19:30:38 -0500

    import footer

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
+import Footer from './components/Footer'
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
@@ -93,7 +93,7 @@ function App() {
               />
             </Routes>
           </div>
-          
+          <Footer/>
         </div>
       </Router>
       </ApolloProvider>
