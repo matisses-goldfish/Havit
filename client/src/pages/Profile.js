134a3bf23639054d69402259ab039712cbe79d80
lizbett <rlizbett21@gmail.com>
2022-03-17 17:29:19 -0500

    profile updated

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,58 +1,61 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 function Profile() {
     return (
         <Box
-            sx={{
-                display: 'flex',
-                '& > :not(style)': {
-                    m: 1,
-                    width: 128,
-                    height: 128,
-                },
-            }}
+        sx={{
+            display: 'flex',
+            '& > :not(style)': {
+                m: 1,
+                width: 128,
+                height: 128,
+            },
+        }}
         >
-            <Paper variant="outlined">
-                <h3>Public Profile</h3>
-                {/*Add User */}
-            </Paper>
-            <Paper variant="outlined" square>
-                <h3>Update Information</h3>
-                <TextField id="outlined-basic" label="Name" variant="outlined" /> 
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                  <h2>Public Profile</h2> 
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {/*User name */} Username
+                    {/*User profile Picture */}
+                </Typography>
+            </CardContent>
+        </Card>
+        <Card sx={{ minWidth: 345 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    <h2>Update Information</h2>
+                </Typography>
+                <TextField id="outlined-basic" label="Name" variant="outlined" />
                 <TextField id="outlined-basic" label="Pronouns" variant="outlined" />
-                <TextField id="outlined-basic" label="Bio" variant="outlined" />
-                <Box
-                    component="form"
-                    sx={{
-                        '& .MuiTextField-root': { m: 1, width: '25ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-
-                    <div>
-                        <TextField
-                            required
-                            id="filled-required"
-                            label="Required"
-                            defaultValue="Email"
-                            variant="filled"
-                        />
-                        <TextField
-                            id="filled-password-input"
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            variant="filled"
-                        />
-                        <Button variant="contained">Update</Button>
-                    </div>
-                </Box>
-            </Paper>
+                <TextField id="outlined-basic" label="Bio" variant="outlined" />   
+                <div>
+                    <TextField
+                        required
+                        id="filled-required"
+                        label="Required"
+                        defaultValue="Email"
+                        variant="filled"
+                    />
+                    <TextField
+                        id="filled-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        variant="filled"
+                    />
+                    <Button variant="contained">Update</Button>
+                </div>
+            </CardContent>
+        </Card>
         </Box>
     );
 }
