a6e6c7cf940551beceebfb530685031e9e23bb9c
sahana <sahanamai@gmail.com>
2022-03-16 19:32:20 -0500

    import Navtab from header

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import NavTab from './components/Header'
 import Footer from './components/Footer'
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -49,6 +49,7 @@ function App() {
 <ApolloProvider client={client}>
       <Router>
         <div className="App">
+          <NavTab />
           <div className="container">
             <Routes>
               <Route 
